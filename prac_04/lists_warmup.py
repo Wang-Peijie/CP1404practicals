dc73ed917a02cf216ff4eeb66e278de0b62cc030
WangCe <3119682363@qq.com>
2025-06-10 13:14:56 +0800

    Add several different answers

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -3,9 +3,15 @@
 #Change the last element of numbers to 1
 numbers = ["ten", 1, 4, 1, 5, 9, 1]
 #Print all the elements from numbers except the first two (slice)
-for number in numbers[3:]:
-    print(number,end=" ")
+print(*numbers[2:])
 #or
-print(",".join([str(n) for n in numbers[3:]]))
+for number in numbers[2:]:
+    print(number,end=" ")
+print()
+#or
+print(" ".join([str(n) for n in numbers[2:]]))
+#or
+[print(n,end=" ")for n in numbers[2:]]
+print()
 #Print whether 9 is an element of numbers
 print(9 in numbers)
