445286a8de5cce6ee6cfa63e39351a19299d6d25
WangCe <3119682363@qq.com>
2025-06-10 14:40:16 +0800

    Finish amost feature

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,20 @@
+"""
+CP1404-prac04
+Make a quick pick program for Lottery Ticket
+"""
+import random
+NUMBER_OF_RANDOM = 6
+MAX_RANDOM_NUMBER = 45
+MIN_RANDOM_NUMBER = 1
+number_of_picks = int(input("How many quick picks?"))
+total_result = []
+for i in range(number_of_picks):
+    one_round_result = []
+    while len(one_round_result) < NUMBER_OF_RANDOM:
+        number = random.randint(MIN_RANDOM_NUMBER,MAX_RANDOM_NUMBER)
+        if number in one_round_result:
+            pass
+        else:
+            one_round_result.append(random.randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER))
+    one_round_result.sort()
+    total_result.append(one_round_result)
