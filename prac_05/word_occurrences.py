897d9bf76c95631361d62661236b37ad680db3c2
WangCe <3119682363@qq.com>
2025-06-19 18:05:43 +0800

    Discovered an error in the code, changed the algorithm inside the loop.Add print part.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,14 +2,19 @@
 a program to count the occurrences of words in a string. The program should ask the user for a string, then print
 the counts of how many of each word are in the string.
 Estimate: 20 minutes
-Actual:    minutes
+Actual:   20 minutes
 """
-word_to_length = {}
+word_to_count = {}
 text = input("Enter a sentence:")
-text_list = text.split()
-for word in text_list:
-    word_to_length[word] = len(word)
-max_word_length = max((len(word)) for word in text_list)
-print(max_word_length)
-# print(word_to_lenght)
+texts = text.split()
+for word in texts:
+    try:
+        word_to_count[word] += 1
+    except KeyError:
+        word_to_count[word] = 1
+max_word_length = max((len(word)) for word in texts)
+# print(max_word_length)
+# print(word_to_count)
 print(f"text = {text}")
+for word in word_to_count:
+    print(f"{word:{max_word_length}} = {word_to_count[word]}")
