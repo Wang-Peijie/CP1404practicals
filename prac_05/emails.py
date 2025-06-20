7bf7fd55d1c3a5ed2cce812da3afbdb80150a987
WangCe <3119682363@qq.com>
2025-06-20 16:57:54 +0800

    add a loop to repeat main function. Put some basic construction

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,3 +3,22 @@ Write a program that stores users' emails (unique keys) and names (values) in a 
 Estimate: 20 minutes
 Actual:
 """
+def main():
+    #stores users' emails (unique keys) and names (values) in a dictionary.
+    key_to_value = {}
+    email = input("Email: ")
+    while email != '':
+        name = extract_name(email)
+        choice = input(f"Is your name {name}? (Y/n)").upper()
+        if choice == "Y" or choice == '':
+            pass
+        else:
+            name = input("Name: ")
+        email = input("Email: ")
+
+
+
+def extract_name(string):
+    #Extract a username from user email string
+
+main()
