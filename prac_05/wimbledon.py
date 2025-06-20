0084a30fc3f6a20f47959853984e84ee42e51e6c
WangCe <3119682363@qq.com>
2025-06-20 19:32:54 +0800

    Complete all functions, start the print part

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -6,22 +6,28 @@ Actual:
 """
 FILENAME = "wimbledon.csv"
 def main():
+    """process the data and display processed information"""
     champion_names = []
     countrys = []
     wimbledon_data = read_file()
     # print(wimbledon_data)
     for line in wimbledon_data:
         champion_names.append(line.strip().split(",")[2])
-        countrys.append(line.strip().split(",")[3])
+        countrys.append(line.strip().split(",")[1])
     # print(champion_names)
     # print(countrys)
     countrys = organize_countrys(countrys)
+    champion_to_win = {}
 
-def calculate_champion_times(data):
-    pass
+
+
+def calculate_champion_times(datas,name):
+    """Calculate how many times each champion won"""
+    number = datas.count(name)
+    return number
 
 def read_file():
-    #read file lines in to a list
+    """read file lines in to a list"""
     data = []
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
@@ -29,6 +35,7 @@ def read_file():
     return data
 
 def organize_countrys(data):
+    """Organize each country that has ever won a championship"""
     data = set(data)
     # print(data)
     return sorted(list(data))
