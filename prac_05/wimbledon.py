084a9e37ac47c6c40f489f51c19f7e4c7d737c33
WangCe <3119682363@qq.com>
2025-06-20 19:45:56 +0800

    add an actual time

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,7 +2,7 @@
 CP1404_prac05
 Write a program to read this file, process the data and display processed information
 Estimate: 20 minutes
-Actual:
+Actual: 40min
 """
 FILENAME = "wimbledon.csv"
 def main():
