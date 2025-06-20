3e60067d317edbd780f5939f8960babd30808d26
WangCe <3119682363@qq.com>
2025-06-20 18:48:22 +0800

    name every function,complete read file function

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,3 +4,22 @@ Write a program to read this file, process the data and display processed inform
 Estimate: 20 minutes
 Actual:
 """
+FILENAME = "wimbledon.csv"
+def main():
+    wimbledon_data = read_file()
+    print(wimbledon_data)
+
+def calculate_total_times(data):
+    pass
+
+def read_file():
+    data = []
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        data = in_file.readlines()
+    return data
+
+def store_country(data):
+    pass
+
+main()
