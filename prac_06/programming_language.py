334391c5adf7a5e3667cced32b28c2b63db056e9
WangCe <3119682363@qq.com>
2025-07-06 20:23:02 +0800

    add the __init__ method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,1 +1,16 @@
 """CP1404/CP5632 Practical - language class."""
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a Car instance.
+
+        name: string, a name of this ProgrammingLanguage object
+        typing: string
+        reflection: bool
+        year: int
+        """
+        self.name = name
+        self.typing = typing
+        self.reflection = is_dynamic(reflection)
+        self.year = year
+
+
