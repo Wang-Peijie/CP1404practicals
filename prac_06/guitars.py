90296443ee17dab129d1d891848f67a89444d808
WangCe <3119682363@qq.com>
2025-07-06 22:18:48 +0800

    add a loop for user input, and store information in guitars list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,2 +1,17 @@
 """CP1404/CP5632 Practical - the client program of guitar."""
 from guitar import Guitar
+print("My guitars!")
+guitars= []
+name = input("Name:")
+while name != "":
+    year = int(input("Year:"))
+    cost = float(input("Cost:"))
+    guitar = Guitar(name, year, cost)
+    print(f"{guitar} added.\n")
+    guitars.append(guitar)
+    name = input("Name:")
+print("... snip ...\n\n")
+print("These are my guitars:")
+
+
+# test: guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
