18a4604586fac133f9f50c7c14bb4cbd7555cd12
WangCe <3119682363@qq.com>
2025-07-06 20:48:57 +0800

    a new list that contains these three existing ProgrammingLanguage objects

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,7 +2,10 @@
 CP1404/CP5632 Practical - language program.
 """
 from programming_language import ProgrammingLanguage
+
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
+
+languages = [python, ruby, visual_basic]
