196d72dc51de18319b5ed5057b5191f41a2b4567
WangCe <3119682363@qq.com>
2025-07-06 21:04:23 +0800

    add __init__ method, initialise a guitar instance.

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,3 +1,14 @@
 """CP1404/CP5632 Practical - guitar class."""
 class Guitar:
     """Represent a Guitar object."""
+    def __init__(self, name='', year=0, cost=0.0):
+        """
+        Initialise a guitar instance.
+
+        name: string, a name of a guitar
+        year: int
+        cost: float
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
