2729e969451fa3b7a2d3b767ca89512cccf9ab94
WangCe <3119682363@qq.com>
2025-07-06 22:28:54 +0800

    add a loop for print all the guitars and print is them vintage.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,17 +1,20 @@
 """CP1404/CP5632 Practical - the client program of guitar."""
 from guitar import Guitar
+
+CURRENT_YEAR = 2025
 print("My guitars!")
 guitars= []
-name = input("Name:")
+name = input("Name: ")
 while name != "":
-    year = int(input("Year:"))
-    cost = float(input("Cost:"))
+    year = int(input("Year: "))
+    cost = float(input("Cost: "))
     guitar = Guitar(name, year, cost)
     print(f"{guitar} added.\n")
     guitars.append(guitar)
-    name = input("Name:")
-print("... snip ...\n\n")
+    name = input("Name: ")
+print("\n... snip ...\n")
 print("These are my guitars:")
-
-
-# test: guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+for i, guitar in enumerate(guitars, 1):
+    vintage_string = "(vintage)" if guitar.is_vintage(CURRENT_YEAR) else ""
+    print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+# for test: guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
