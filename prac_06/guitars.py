f0dd85e5680e9cd63406603694d7534333c625af
WangCe <3119682363@qq.com>
2025-07-06 22:31:16 +0800

    add some docstrings

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -6,6 +6,7 @@ print("My guitars!")
 guitars= []
 name = input("Name: ")
 while name != "":
+    """exit when user enter a blank name"""
     year = int(input("Year: "))
     cost = float(input("Cost: "))
     guitar = Guitar(name, year, cost)
@@ -15,6 +16,7 @@ while name != "":
 print("\n... snip ...\n")
 print("These are my guitars:")
 for i, guitar in enumerate(guitars, 1):
+    """print with i (the index) and guitar (the element)"""
     vintage_string = "(vintage)" if guitar.is_vintage(CURRENT_YEAR) else ""
     print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 # for test: guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
