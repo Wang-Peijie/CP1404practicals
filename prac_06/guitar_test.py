eaec10ca64dac9052faef7bf7efe13a94a351b01
WangCe <3119682363@qq.com>
2025-07-06 21:34:24 +0800

    test get_age method

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,2 +1,10 @@
 """CP1404/CP5632 Practical - guitar tast."""
 from guitar import Guitar
+
+CURRENT_YEAR = 2025
+
+guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+guitar2 = Guitar("Another Guitar", 2013, 10000.00)
+
+print(f"{guitar1.name} get_age() - Expected 103. Got {guitar1.get_age(CURRENT_YEAR)}")
+print(f"{guitar2.name} get_age() - Expected 12. Got {guitar2.get_age(CURRENT_YEAR)}")
