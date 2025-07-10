e2c6c2b7a1f6c64959e3d078bf3e19f51c1e6b6c
WangCe <3119682363@qq.com>
2025-07-11 07:41:24 +0800

    Add __init__ and __repr__ methods

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,2 +1,17 @@
 """CP1404/CP5632 Practical 07 - Project class"""
+from datetime import  date
 class Project:
+    """"""
+
+    def __init__(self, name, start_date, priority, cost, percentage):
+        """"""
+        self.name = name
+        self.start_date = start_date
+        self.priorty = priority
+        self.cost = cost
+        self.percentage = percentage
+
+
+    def __repr__(self):
+        """"""
+        return f"{self.name} {self.start_date} {self.priorty} {self.cost} {self.percentage}"
