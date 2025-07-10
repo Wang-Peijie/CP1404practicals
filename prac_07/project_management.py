65dca65ce7ec4ab19afcdf7bda5334dea9bbcf29
WangCe <3119682363@qq.com>
2025-07-11 07:52:22 +0800

    Add main function and import class and date module

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,2 +1,23 @@
-"""CP1404/CP5632 Practical 07 - Project management"""
+"""
+CP1404/CP5632 Practical 07 - Project management
+Estimated time:2 hour
+Actual time: 3 hour
+"""
 from project import Project
+from datetime import date
+MENU = ("- (L)oad projects\n"
+        "- (S)ave projects)\n"
+        "- (D)isplay projects\n"
+        "- (F)ilter projects by date\n"
+        "- (A)dd new project\n"
+        "- (U)pdate project\n"
+        "- (Q)uit\n"
+        ">>>")
+def main():
+    """"""
+    print("Welcome to Pythonic Project Management")
+
+    print(MENU)
+
+main()
+
