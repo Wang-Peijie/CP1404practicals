67c16ed111d8f526b9e6b586f66f2cfaf2920461
WangCe <3119682363@qq.com>
2025-07-19 04:10:51 +0800

    Finish the Requirement

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -1,7 +1,7 @@
 """
 CP1404/CP5632 Practical
 Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
+Wang Peijie
 Started 13/10/2015
 """
 
@@ -9,7 +9,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
+__author__ = 'Wang Peijie'
 
 
 class SquareNumberApp(App):
