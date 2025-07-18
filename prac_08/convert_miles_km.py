755052ee2c4d4990f082c151c5489da03cd25efd
WangCe <3119682363@qq.com>
2025-07-19 05:02:32 +0800

    finish the requirements

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -22,9 +22,23 @@ class MilesConvertKmApp(App):
 
     def handle_calculation(self):
         """ handle calculation get input number for input_mile and convert to string"""
-        value = self.root.ids.input_mile.text
+        value = self.handle_valid_checking()
         result = MILES_TO_KM * value
         self.root.ids.StringProperty.text = str(result)
+
+    def handle_valid_checking(self):
+        """handle valid to check input number is correct"""
+        try:
+            value = float(self.root.ids.input_mile.text)
+            return value
+        except ValueError:
+            return 0
+
+    def handle_plus_minus(self,change):
+        """handle plus and minus bottom to let input number +1 or -1"""
+        value = self.handle_valid_checking() + change
+        self.root.ids.input_mile.text = str(value)
+        self.handle_calculation()
 
 
 MilesConvertKmApp().run()
