abf1d8ea0aa819299cec3511f91efc206d2ffa76
WangCe <3119682363@qq.com>
2025-07-19 05:39:24 +0800

    Completed the basic setup of the method.

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,6 +1,33 @@
 """
 CP1404/CP5632 Practical
-Miles to Kilometres Converter
+dynamic labels
 Wang Peijie
 Started 19/7/2025
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+NAMES = ["Alice", "Bob", "Charlie", "Diana"]
+class DynamicWidgetsApp(App):
+    """Dynamic widgets kivy app to dynamic widget creation."""
+
+
+    def __init__(self, **kwargs):
+        """initialize main app."""
+        super().__init__(**kwargs)
+        self.names = NAMES
+
+    def build(self):
+        """Build the kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        """Create labels widgets from the NAMES list """
+
+
+DynamicWidgetsApp().run()
