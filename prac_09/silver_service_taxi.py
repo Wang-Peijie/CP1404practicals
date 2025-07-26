6bcc51450fe269f0b8862ed1d90d97cbcb8679d1
WangCe <3119682363@qq.com>
2025-07-27 03:15:00 +0800

    add __str__ method and run some test in Silver_Service_Taxi_test

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -6,8 +6,14 @@ from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Specialised version of a Taxi that allows SilverServiceTaxi to have a different effective price_per_km."""
-    def __init__(self, name, fuel, price_per_km):
+
+    def __init__(self, name, fuel, fanciness:float):
         """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
         super().__init__(name, fuel)
         self.current_fare_distance = 0
-        self.price_per_km = price_per_km
+        self.price_per_km = self.price_per_km * fanciness
+
+    def __str__(self):
+        """Return a string like a taxi but with current fare distance."""
+        return f"{super().__str__()}"
+
--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -3,3 +3,6 @@ CP1404/CP5632 Practical
 SilverServiceTaxi test
 """
 from silver_service_taxi import SilverServiceTaxi
+
+taxi01 = SilverServiceTaxi("Rad car", 100 ,2)
+print(taxi01)
