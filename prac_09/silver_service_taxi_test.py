11baa8eaa653627700e6a4030ef8783f045ec5b3
WangCe <3119682363@qq.com>
2025-07-27 03:37:58 +0800

    fare is not a number multiple of 10c

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -8,4 +8,4 @@ taxi01 = SilverServiceTaxi("Rad car", 100 ,2)
 print(taxi01)
 taxi01.drive(18)
 actual_fare = taxi01.get_fare()
-assert actual_fare == 48.78, actual_fare
+assert actual_fare == 48.8, actual_fare
