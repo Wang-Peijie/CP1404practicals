ad93202b421a8035892c785b859844fc3b186953
WangCe <3119682363@qq.com>
2025-07-27 04:27:19 +0800

    add basic menu loop and valid check

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,6 +1,26 @@
 """
 CP1404/CP5632 Practical
-Car class
+taxi simulator
 """
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive\n>>>"
+
+def main():
+    """a function to run a taxi simulator"""
+    current_taxi = None
+    total_bill = 0.0
+    print("Let's drive!")
+    choice = input(MENU).upper()
+    while choice != "Q":
+        if choice == "C":
+            pass
+        if choice == "D":
+            pass
+        else:
+            print("Invalid option")
+
+        print(f"Bill to date: ${total_bill:.2f}")
+        choice = input(MENU).upper()
+
