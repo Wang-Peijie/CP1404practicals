fc0e56ad5e20bb004df747d692b9f09ad02f1fa7
WangCe <3119682363@qq.com>
2025-07-27 02:43:23 +0800

    Create UnreliableCar class with three methods __init__, __str__ and drive

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,22 @@
+"""CP1404/CP5632 Practical - unreliable class (inherits from Car)."""
+from car import Car
+import random
+
+class UnreliableCar(Car):
+    """Specialised version of a Car"""
+    def __init__(self, name, fuel, reliability):
+        """Initialise an Unreliable instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+        self.current_distance = 0
+
+
+    def drive(self, distance):
+        """Generate a random number only drive the car if that number is less than the car's reliability"""
+        if random.randint(0, 100) < self.reliability:
+            distance_driven = super().drive(distance)
+            self.current_distance += distance_driven
+
+    def __str__(self):
+        """Return a string like a Car but with current fare distance."""
+        return f"{super().__str__()}, {self.current_distance}km on current fare"
