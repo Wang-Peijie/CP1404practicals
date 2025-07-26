4d1bf78e09978af61e74b8e014c4d70a6afd440f
WangCe <3119682363@qq.com>
2025-07-27 03:58:17 +0800

    add method to implement refactor and add __init__ method

--- prac_09/band.py
+++ prac_09/band.py
@@ -2,3 +2,19 @@
 CP1404/CP5632 Practical
 band class
 """
+from musician import Musician
+from guitar import Guitar
+
+class Band:
+    """a Band includes musicians"""
+    def __init__(self, name=""):
+        """Initialize a Band with a name and an empty list of musicians."""
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        """Return a format string to show musicians"""
+
+    def add(self, musician):
+        """Add a musician to the band's list of musicians."""
+        self.musicians.append(musician)
