16be8d5473a4e2ab66624111115520ccf4563ba6
WangCe <3119682363@qq.com>
2025-07-27 03:02:01 +0800

    start!

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,7 @@
+"""
+CP1404/CP5632 Practical
+SilverServiceTaxi -> taxi -> Car class
+"""
+from taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,5 @@
+"""
+CP1404/CP5632 Practical
+SilverServiceTaxi test
+"""
+from silver_service_taxi import SilverServiceTaxi
