cd5fbbe59715f15e0b93fe27d8e276384a7006a5
WangCe <3119682363@qq.com>
2025-07-27 03:30:18 +0800

    Use assert tests

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -6,3 +6,6 @@ from silver_service_taxi import SilverServiceTaxi
 
 taxi01 = SilverServiceTaxi("Rad car", 100 ,2)
 print(taxi01)
+taxi01.drive(18)
+actual_fare = taxi01.get_fare()
+assert actual_fare == 48.78, actual_fare
