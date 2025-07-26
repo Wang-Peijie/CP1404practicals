ead3e9af1556126743bff41c27f82115c37b9eaa
WangCe <3119682363@qq.com>
2025-07-27 03:04:46 +0800

    add __init__ method

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -5,3 +5,9 @@ SilverServiceTaxi -> taxi -> Car class
 from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that allows SilverServiceTaxi to have a different effective price_per_km."""
+    def __init__(self, name, fuel, price_per_km):
+        """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
+        super().__init__(name, fuel)
+        self.current_fare_distance = 0
+        self.price_per_km = price_per_km
